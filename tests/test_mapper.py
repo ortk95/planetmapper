ef59f0b19805eec11e208a8c0bf7e362a30f47c6
Oliver King <oliver.king95@gmail.com>
2022-10-24 12:36:18 +0100

    Adding tests for xy<->latlon conversion

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -34,5 +34,37 @@ class TestSpiceBody(unittest.TestCase):
         self.assertTrue(all(np.isnan(self.body.radec_to_lonlat(ra, dec))))
 
 
+class TestObservation(unittest.TestCase):
+    def setUp(self):
+        dtm = datetime.datetime.now()
+        dtm_str = dtm.strftime('%Y-%m-%d %H:%M:%S')
+        self.observation = mapper.Observation('jupiter', dtm_str)
+
+    def test_round_trip_conversion(self):
+        """
+        Test that conversion lon/lat -> ra/dec -> lon/lat gives consistent result.
+        """
+        obs = self.observation
+        while True:
+            # Choose random point on surface that's visible
+            lon = np.deg2rad(np.random.rand() * 360)
+            lat = np.deg2rad(np.random.rand() * 180 - 90)
+            if obs.test_if_lonlat_visible(lon, lat):
+                break
+        lon_rt, lat_rt = obs.xy_to_lonlat(*obs.lonlat_to_xy(lon, lat))
+        self.assertAlmostEqual(lon, lon_rt, places=3)
+        self.assertAlmostEqual(lat, lat_rt, places=3)
+
+    def test_missing(self):
+        """
+        Test ra/dec that should miss the target.
+        """
+        x = self.observation.get_x0()
+        y = self.observation.get_x0()
+        x = x + self.observation.get_r0() * 10
+
+        self.assertTrue(all(np.isnan(self.observation.xy_to_lonlat(x, y))))
+
+
 if __name__ == '__main__':
     unittest.main()
