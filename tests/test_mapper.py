7010c440c94dfa0cd3e2aac52a7c59bf39c0c712
Oliver King <oliver.king95@gmail.com>
2022-10-26 12:24:39 +0100

    Updating tests

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -29,18 +29,28 @@ class TestBody(unittest.TestCase):
 
     def test_distance(self):
         self.assertAlmostEqual(
-            self.body.target_distance, np.linalg.norm(self.body._target_obsvec)
+            self.body.target_distance,
+            np.linalg.norm(self.body._target_obsvec),
+            places=3,
         )
         self.assertAlmostEqual(
-            self.body.subpoint_distance, np.linalg.norm(self.body._subpoint_rayvec)
+            self.body.subpoint_distance,
+            np.linalg.norm(self.body._subpoint_rayvec),
+            places=3,
         )
+
+    def test_radians2degrees(self):
+        self.assertEqual(self.body._degree_pair2radians(0, 180), (0, np.pi))
+
+    def test_degrees2radians(self):
+        self.assertEqual(self.body._radian_pair2degrees(0, np.pi), (0, 180))
 
 
 class TestObservation(unittest.TestCase):
     def setUp(self):
         dtm = datetime.datetime.now()
         dtm_str = dtm.strftime('%Y-%m-%d %H:%M:%S')
-        self.observation = mapper.Observation('jupiter', dtm_str)
+        self.observation = mapper.Observation('saturn', dtm_str)
 
     def test_round_trip_conversion(self):
         """
@@ -73,10 +83,10 @@ def generate_lonlat(body: mapper.Body) -> tuple[float, float]:
         # Avoid lons near meridian where wraparound (e.g. 359.9999<->0.0001) can cause
         # unit tests to incorrectly fail
         lon = np.deg2rad(rng.random() * 358 + 1)
-        
+
         # Avoid polar latitudes where singularity can break 1-to-1 conversions
         lat = np.deg2rad(rng.random() * 120 - 60)
-        
+
         if body.test_if_lonlat_visible(lon, lat):
             return lon, lat
 
