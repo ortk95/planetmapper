19a4a398563211a542f3ccc9aa0f3e271a9a07c4
Oliver King <oliver.king95@gmail.com>
2022-11-02 21:37:38 +0000

    Documentation

--- planetmapper/mapper.py
+++ planetmapper/mapper.py
@@ -1077,8 +1077,9 @@ class BodyXY(Body):
     centre of the planet's disc `(x0, y0)`, the (equatorial) pixel radius of the disc
     `r0` and the `rotation` of the disc. These values can be adjusted using methods such
     as :func:`set_x0` and retrieved using methods such as :func:`get_x0`. It is
-    important to note that conversions involving `xy` image pixel coordinates will
-    produce different results before and after these values are adjusted.
+    important to note that conversions involving `xy` image pixel coordinates (e.g.
+    backplane image generatiton) will produce different results before and after these
+    values are adjusted.
 
     The size of the image can be specified by using the `nx` and `ny` parameters to
     specify the number of pixels in the x and y dimensions of the image respectively.
@@ -1380,7 +1381,7 @@ class BodyXY(Body):
     def get_r0(self) -> float:
         """
         Returns:
-            equatorial radius in pixels of the target body.
+            Equatorial radius in pixels of the target body.
         """
         return self._r0
 
@@ -1903,9 +1904,51 @@ class BodyXY(Body):
 
 
 class Observation(BodyXY):
+    """
+    Class representing an actual observation of an astronomical body at a specific time.
+
+    This is a subclass of :class:`BodyXY`, with additional methods to interact with the
+    observed data, such as by saving a FITS file containing calculated backplane data.
+    All methods described in :class:`BodyXY`, :class:`Body` and :class:`SpiceTool` are
+    therefore available in instances of this class.
+
+    This class can be created by either providing a `path` to a data file to be loaded,
+    or by directly providing the `data` itself  (and optionally a FITS `header`). The
+    `nx` and `ny` values for :class:`BodyXY` will automatically be calculated from the
+    input data.
+
+    If the input data is a FITS file (or a `header` is specified), then this class will
+    attempt to generate appropriate parameters (e.g. `target`, `utc`) by using the
+    values in the header. This allows an instance of this class to be created with a
+    single argument specifying the `path` to the FITS file e.g.
+    `Observation('path/to/file.fits')`. Manually specified parameters will take
+    precedence, so `Observation('path/to/file.fits', target='JUPITER')` will have Jupiter
+    as a target, regardless of any values saying otherwise in the FITS header.
+
+    If a FITS header is not provided (e.g. if the input path corresponds to an image
+    file), then at least the `target` and `utc` parameters need to be specified.
+
+    Args:
+        path: Path to data file to load. If this is `None` then `data` must be specified
+            instead.
+        data: Array containing observation data to use instead of loading the data from
+            `path`. This should only be provided if `path` is None.
+        header: FITS header which corresponds to the provided `data`. This is optional
+            and should only be provided if `path` is None.
+        target: Name of target body, passed to :class:`Body`. If this is unspecified,
+            then the target will be derived from the values in the FITS header.
+        utc: Time of observation, passed to :class:`Body`. If this is unspecified,
+            then the time will be derived from the values in the FITS header.
+        **kwargs: Additional parameters are passed to :class:`BodyXY`. These can be used
+            to specify additional parameters such as`observer`.
+    """
+
     FITS_FILE_EXTENSIONS = ('.fits', '.fits.gz')
+    """File extensions which will be read as FITS files."""
     IMAGE_FILE_EXTENSIONS = ('.png', '.jpg', '.jpeg')
+    """File extensions which will be read as image files."""
     FITS_KEYWORD = 'PLANMAP'
+    """FITS keyword used in metadata added to header of output FITS files."""
 
     def __init__(
         self,
@@ -1913,11 +1956,28 @@ class Observation(BodyXY):
         *,
         data: np.ndarray | None = None,
         header: fits.Header | None = None,
+        target: str | None = None,
+        utc: str | datetime.datetime | None = None,
         **kwargs,
     ) -> None:
-        self.path = path
-        self.header: fits.Header = None  # type: ignore
+        # Add docstrings
         self.data: np.ndarray
+        """Observed data."""
+        self.header: fits.Header
+        """
+        FITS header containing data about the observation. If this is not provided, then
+        a basic header will be produced containing data derived from the `target` and 
+        `utc` parameters.
+        """
+
+        self.path = path
+        self.header = None  # type: ignore
+
+        # Add optional kw to kwargs if specified
+        if target is not None:
+            kwargs['target'] = target
+        if utc is not None:
+            kwargs['utc'] = utc
 
         # TODO add warning about header being modified in place? Or copy header?
         if self.path is None:
@@ -1925,6 +1985,7 @@ class Observation(BodyXY):
                 raise ValueError('Either `path` or `data` must be provided')
             self.data = data
         else:
+            # TODO should we have a way to provide both path and header for e.g. JPEGS?
             if data is not None:
                 raise ValueError('`path` and `data` are mutually exclusive')
             if header is not None:
@@ -1988,7 +2049,14 @@ class Observation(BodyXY):
 
     # Auto disc id
     def centre_disc(self) -> None:
-        """Centre disc and make it fill ~90% of the observation"""
+        """
+        Centre the target's planetary disc and make it fill ~90% of the observation.
+
+        This adjusts `x0` and `y0` so that they lie in the centre of the image, and `r0`
+        is adjusted so that the disc fills 90% of the shortest side of the image. For
+        example, if `nx = 20` and `ny = 30`, then `x0` will be set to 10, `y0` will be
+        set to 15 and `r0` will be set to 9. The rotation of the disc is unchanged.
+        """
         self.set_x0(self._nx / 2)
         self.set_y0(self._ny / 2)
         self.set_r0(0.9 * (min(self.get_x0(), self.get_y0())))
@@ -2002,12 +2070,30 @@ class Observation(BodyXY):
         comment: str | None = None,
         hierarch_keyword: bool = True,
     ):
+        """
+        Add a card to the FITS :attr:`header`. This is mainly used to record metadata
+        which is then saved in the header of any FITS files saved by :func:`save`. By
+        default, the keyword is modified to provide a consistent keyword prefix for all
+        header cards added by this routine.
+
+        Args:
+            keyword: Card keyword.
+            value: Card value.
+            comment: Card comment. If unspecified not comment will be added.
+            hierarch_keyword: Toggle adding the keyword prefix from :attr:`FITS_KEYWORD`
+                to the keyword.
+        """
         if hierarch_keyword:
             keyword = f'HIERARCH {self.FITS_KEYWORD} {keyword}'
         with utils.filter_fits_comment_warning():
             self.header.append(fits.Card(keyword=keyword, value=value, comment=comment))
 
     def add_header_metadata(self):
+        """
+        Add automatically generated metadata to :attr:`header`. This is automatically
+        called by :func:`save` so `add_header_metadata` does not normally need to be
+        called manually.
+        """
         self.append_to_header('VERSION', common.__version__, 'Planet Mapper version.')
         self.append_to_header('URL', common.__url__, 'Webpage.')
         self.append_to_header(
@@ -2097,7 +2183,21 @@ class Observation(BodyXY):
         )
 
     def save(self, path: str) -> None:
-        """Save fits file with backplanes"""
+        """
+        Save a FITS file containing the observed data and generated backplanes.
+
+        The primary HDU in the FITS file will be the :attr:`data` and :attr:`header`
+        of the observed data, with appropriate metadata automatically added to the
+        header by :func:`add_header_metadata`. The backplanes are generated from all the
+        registered backplanes in :attr:`BodyXY.backplanes` and are saved as additional
+        HDUs in the FITS file.
+
+        For larger image sizes, the backplane generation can be slow, so this function
+        may take some time to complete.
+
+        Args:
+            path: Filepath of output file.
+        """
         with utils.filter_fits_comment_warning():
             self.add_header_metadata()
 
@@ -2113,7 +2213,18 @@ class Observation(BodyXY):
                 hdul.append(hdu)
             hdul.writeto(path, overwrite=True)
 
-    def make_filename(self, extension='.fits.gz') -> str:
+    def make_filename(self, extension='.fits') -> str:
+        """
+        Automatically generates a useful filename from the target name and date of the
+        observation, e.g. `'JUPITER_2000-01-01T123456.fits.gz'`.
+
+        Args:
+            extension: Optionally specify the file extension to add to the filename.
+
+        Returns:
+            Filename built from the target name and observation date.
+
+        """
         return '{target}_{date}{extension}'.format(
             target=self.target,
             date=self.dtm.strftime('%Y-%m-%dT%H%M%S'),
