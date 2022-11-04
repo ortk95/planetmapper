42038a137b964bcac12b5dd289d7e0d8ab164b1d
Oliver King <oliver.king95@gmail.com>
2022-11-04 16:39:28 +0000

    Adding script to scrape ring radii from planetary factsheets

--- scratchpad.py
+++ scratchpad.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Script for testing stuff during development (TODO delete in final version)"""
-# import planetmapper
+import planetmapper
 import numpy as np
 import numpy.ma as ma
+
 # import tools
 # import datetime
 
 
-
-
-y = ma.array([1, 2, 3], mask = [1, 1, 0])
+y = ma.array([1, 2, 3], mask=[1, 1, 0])
