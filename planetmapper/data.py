0851c7e57e9dec635e42a74c7ad76e84bd71ab70
Oliver King <oliver.king95@gmail.com>
2022-11-04 16:51:03 +0000

    Docs

--- planetmapper/data.py
+++ planetmapper/data.py
@@ -4,11 +4,31 @@ import functools
 
 
 def make_data_path(filename: str) -> str:
+    """
+    Generates a path to a static data file.
+
+    Args:
+        filename: Filename of the data file stored in planetmapper/data
+
+    Returns:
+        Absolute path to the data file.
+    """
     data_dir = os.path.join(os.path.split(__file__)[0], 'data')
     return os.path.join(data_dir, filename)
 
 
 @functools.lru_cache
-def get_ring_radii():
+def get_ring_radii() -> dict[str, dict[str, list[float]]]:
+    """
+    Load planetary ring radii from data file.
+
+    Returns:
+        Dictionary where the keys are planet names and the values are dictionaries
+        contianing ring data. These ring data dictionaries have keys corresponding to
+        the names of the rings, and values with a list of ring radii in km. If the
+        length of this list is 2, then the values give the inner and outer radii of the
+        ring respectively. Otherwise, the length should be 1, meaning the ring has a 
+        single radius.
+    """
     with open(make_data_path('rings.json'), 'r') as f:
         return json.load(f)
