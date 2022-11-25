226f950f88e0147a6674e97e072ae3e5820b32ee
Oliver King <oliver.king95@gmail.com>
2022-11-25 18:02:01 +0000

    Moving requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,41 @@
 import os
+import sys
 import setuptools
-import planetmapper.common
 
 root = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(root, 'planetmapper'))
+import common
+
 with open(os.path.join(root, 'README.md'), 'r') as f:
     long_description = f.read()
 
-with open(os.path.join(root, 'requirements.txt'), 'r') as f:
-    requirements = [l.strip() for l in f.readlines()]
-    requirements = [l for l in requirements if l]
-
 setuptools.setup(
     name='planetmapper',
-    version=planetmapper.common.__version__,
-    author=planetmapper.common.__author__,
+    version=common.__version__,
+    author=common.__author__,
     author_email='oliver.king95@gmail.com',
     description='A Python module for navigating and mapping Solar System observations',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url=planetmapper.common.__url__,
+    url=common.__url__,
     download_url='https://pypi.org/project/planetmapper/',
-    install_requires=requirements,
-    python_requires='>=3.10.0',
     packages=['planetmapper'],
     package_dir={'planetmapper': 'planetmapper'},
     package_data={'planetmapper': ['data/*.json']},
     include_package_data=True,
     project_urls={
         'Documentation': 'https://planetmapper.readthedocs.io/',
-        'GitHub': planetmapper.common.__url__,
+        'GitHub': common.__url__,
     },
+    python_requires='>=3.10.0',
+    install_requires=[
+        'astropy',
+        'matplotlib',
+        'numpy',
+        'Pillow',
+        'spiceypy',
+        'scipy',
+        'photutils',
+        'tqdm',
+    ],
 )
