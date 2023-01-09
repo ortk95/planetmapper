f7d43f03249f7d33b149b1292240c4621ec7a3ba
Oliver King <oliver.king95@gmail.com>
2023-01-09 14:37:11 +0000

    Don't raise error on bad WCS transformation from GUI

--- scratchpad.py
+++ scratchpad.py
@@ -4,8 +4,7 @@
 import planetmapper
 import matplotlib.pyplot as plt
 
-p = '/Users/ortk1/Dropbox/PhD/data/jwst/NIRSPEC_IFU/2023-01-08_Uranus/lon2/data/level2_nav/jw01248004001_03101_00001_nrs1_s3d_nav.fits'
-observation = planetmapper.Observation(p, aberration_correction='CN')
-observation.disc_from_wcs()
-observation.plot_wireframe_km()
-plt.show()
+p = '/Users/ortk1/Desktop/iew608inq_drz.fits'
+observation = planetmapper.Observation(p, aberration_correction='CN', target='uranus')
+observation.disc_from_wcs(True, False)
+observation.run_gui()
