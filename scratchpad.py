1d9e42f85112ca9184aaa516e1670e97fce724fe
Oliver King <oliver.king95@gmail.com>
2023-01-09 12:41:15 +0000

    Update unit tests

--- scratchpad.py
+++ scratchpad.py
@@ -6,6 +6,6 @@ import matplotlib.pyplot as plt
 
 p = '/Users/ortk1/Dropbox/PhD/data/jwst/NIRSPEC_IFU/2023-01-08_Uranus/lon2/data/level2_nav/jw01248004001_03101_00001_nrs1_s3d_nav.fits'
 observation = planetmapper.Observation(p, aberration_correction='CN')
-observation.disc_from_wcs(True, False)
+observation.disc_from_wcs()
 observation.plot_wireframe_xy()
 plt.show()
