9e89b6c58c2db0cf21139793739da08dc82ff057
Oliver King <oliver.king95@gmail.com>
2023-01-09 13:34:47 +0000

    Applying xy fixes to km coordinate system

--- scratchpad.py
+++ scratchpad.py
@@ -7,5 +7,5 @@ import matplotlib.pyplot as plt
 p = '/Users/ortk1/Dropbox/PhD/data/jwst/NIRSPEC_IFU/2023-01-08_Uranus/lon2/data/level2_nav/jw01248004001_03101_00001_nrs1_s3d_nav.fits'
 observation = planetmapper.Observation(p, aberration_correction='CN')
 observation.disc_from_wcs()
-observation.plot_wireframe_xy()
+observation.plot_wireframe_km()
 plt.show()
