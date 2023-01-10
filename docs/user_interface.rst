a707c70150cc26e202a6c496cd21d4bfe6f32608
Oliver King <oliver.king95@gmail.com>
2023-01-10 16:18:12 +0000

    Changing default aberration correction to `CN`
    Provides significantly better consistency with the WCS information in e.g. HST, JWST, ESO headers

--- docs/user_interface.rst
+++ docs/user_interface.rst
@@ -80,7 +80,7 @@ This simple example shows how you could use :func:`planetmapper.Observation.run_
 
     for path in sorted(glob.glob('data/*.fits')):
         # Running from Python allows you to customise SPICE settings like the aberration correction
-        observation = planetmapper.Observation(path, aberration_correction='CN')
+        observation = planetmapper.Observation(path, aberration_correction='CN+S')
 
         # Run some custom setup
         observation.add_other_bodies_of_interest('Io', 'Europa', 'Ganymede', 'Callisto')
--- scratchpad.py
+++ scratchpad.py
@@ -3,7 +3,32 @@
 """Script for testing stuff during development"""
 import planetmapper
 import matplotlib.pyplot as plt
+import astropy.wcs
+import numpy as np
+
+# gui = planetmapper.gui.GUI()
+# gui.run()
 
 p = '/Users/ortk1/Desktop/iew608inq_drz.fits'
-observation = planetmapper.Observation(p, aberration_correction='CN', target='uranus')
-observation.run_gui()
+p = '/Users/ortk1/Dropbox/PhD/data/GBdata/muse/preview/ADP.2019-10-10T21:55:21.853.fits'
+observation = planetmapper.Observation(p, target='io', aberration_correction='CN')
+observation.plot_wireframe_radec()
+plt.show()
+
+header = observation.header
+# wcs = observation._get_wcs_from_header()
+wcs = astropy.wcs.WCS(header).celestial
+
+ra, dec = observation.limb_radec()
+
+img = np.nanmean(observation.data, axis=0)
+plt.imshow(img, origin='lower', cmap='Greys_r')
+x, y = np.array(wcs.world_to_pixel_values(np.array([ra, dec]).T)).T
+plt.plot(x, y)
+
+observation.disc_from_wcs()
+x, y = observation.limb_xy()
+plt.plot(x, y)
+
+
+plt.show()
