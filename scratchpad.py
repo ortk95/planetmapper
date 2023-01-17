6b9247a6748f823e2259fedada2d2cee2d19a60d
Oliver King <oliver.king95@gmail.com>
2023-01-17 11:11:27 +0000

    Adding RA/Dec offset method

--- scratchpad.py
+++ scratchpad.py
@@ -7,6 +7,13 @@ import astropy.wcs
 import numpy as np
 import spiceypy as spice
 
-body = planetmapper.Body('uranus')
-body.add_satellites_to_bodies_of_interest()
-body.plot_wireframe_radec()
+p = '/Users/ortk1/Dropbox/PhD/data/jwst/Uranus_2023jan08/lon2/stage3/d1/Level3_ch4-short_s3d.fits'
+p = '/Users/ortk1/Dropbox/PhD/data/jwst/NIRSPEC_IFU/2023-01-08_Uranus/lon2/data/level2_nav/jw01248004001_03103_00004_nrs1_s3d_nav.fits'
+body = planetmapper.Observation(p)
+body.add_arcsec_offset(0,1)
+ax = body.plot_wireframe_xy()
+img = np.nanmedian(body.data, axis=0)
+ax.imshow(img, origin='lower')
+plt.show()
+
+
