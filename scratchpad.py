61b8a9c3fa3bdba0dc266430bd31ee6d6a9a275b
Oliver King <oliver.king95@gmail.com>
2023-03-30 15:22:41 +0100

    Adding unit tests

--- scratchpad.py
+++ scratchpad.py
@@ -4,9 +4,6 @@
 import planetmapper
 import matplotlib.pyplot as plt
 import numpy as np
-import datetime
-import planetmapper.progress
-import scipy.interpolate
 
 p = '/Users/ortk1/Downloads/URA_NIRSPEC_F290LP_G395H_NRS2_Lon3_2_nav.fits'
 
